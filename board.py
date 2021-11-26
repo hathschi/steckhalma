b41ddfb739473007be9b707685cafbcb5abbd39b
Thomas Schindler <thomas.schindler@mayflower.de>
2021-11-26 07:35:30 +0100

    initial commit, board check moves + tests

---
+++ board.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+
+class Board():
+    _distance_right = np.asarray([2,0])
+    _distance_left = np.asarray([-2,0])
+    _distance_up = np.asarray([0,2])
+    _distance_down = np.asarray([0,-2])
+
+    def __init__(self) -> None:
+        self._board = np.zeros((7,7))
+        #TODO improve. this is not pythonic
+        for i in range(7):
+            for j in range(7):
+                self._board[i,j] = int(self.is_on_board([i,j]))
+        
+        self._board[3,3] = 0
+
+    def is_valid_move(self,start,target):
+        start = np.asarray(start)
+        target = np.asarray(target)
+        distance = target - start
+        valid_distance = np.all(self._distance_right == distance) or np.all(self._distance_left == distance) or np.all(self._distance_up == distance) or np.all(self._distance_down == distance)
+        if not valid_distance:
+            return False
+        
+        mid = ((start + target) / 2).astype(int)
+        board_start = self._board[start[0],start[1]]
+        occupied_start = 1 == board_start
+        valid_start = self.is_on_board(start) and occupied_start
+        valid_mid   = self.is_on_board(mid) and 1 == self._board[mid[0],mid[1]]
+        valid_target = self.is_on_board(target) and 0 == self._board[target[0],target[1]]
+
+        return valid_start and valid_mid and valid_target
+
+
+
+    def is_on_board(self,coordinate):
+        if 0 > coordinate[0] or coordinate[0] >= 7:
+            return False
+        if 0 > coordinate[1] or coordinate[1] >= 7:
+            return False
+        
+        if 2 <= coordinate[0] < 5:
+            return True
+        if 2 <= coordinate[1] < 5:
+            return True
+        
+        return False
+
+    def are_on_board(self,coordinates):
+        #TODO improve. this is not pythonic
+        return np.array([self.is_on_board(coordinate) for coordinate in coordinates])
+
+
---
+++ tests/test_board.py
@@ -0,0 +1,78 @@
+import numpy as np
+import unittest
+
+from board import Board
+
+class TestBoard(unittest.TestCase):
+    _board = Board()
+    _board._board[2,3] = 0
+    _board._board[3,5] = 0
+    
+    def test_are_coordinates(self):
+        coordinates = np.array([
+            [-1,-1],
+            [0,0],
+            [1,1],
+            [2,2],
+            [3,3],
+            [4,4],
+            [5,5],
+            [6,6],
+            [7,7],
+            [1,2],
+            [2,1],
+            [5,1],
+            [1,5],
+            [5,5]
+        ])
+
+        expectation = np.array([
+            False,
+            False,
+            False,
+            True,
+            True,
+            True,
+            False,
+            False,
+            False,
+            True,
+            True,
+            False,
+            False,
+            False
+        ])
+
+        actual = self._board.are_on_board(coordinates)
+
+        assert np.all(expectation == actual)
+    
+    def test_is_valid_move(self):
+        moves = [
+            [[0,3],[2,3]],# move right
+            [[5,3],[3,3]],# move left
+            [[2,1],[2,3]],# move up
+            [[2,5],[2,3]],# move down
+            [[1,5],[3,5]],# start off board
+            [[3,0],[1,0]],# target off board
+            [[3,3],[5,3]],# start empty
+            [[1,3],[3,3]],# mid empty
+            [[6,2],[4,2]],# target occupied
+            [[4,3],[3,3]] # wrong distance
+        ]
+
+        expectations = [
+            True,
+            True,
+            True,
+            True,
+            False,
+            False,
+            False,
+            False,
+            False,
+            False
+        ]
+
+        for i,move in enumerate(moves):
+            assert expectations[i] == self._board.is_valid_move(move[0],move[1])
